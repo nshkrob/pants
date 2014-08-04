8fe14ffd751edb4e7e83332cf52c4c0ffb5ef0f9
Benjy <benjy@foursquare.com>
2014-08-04 22:12:13 +0000

    Address code review comments.

--- src/python/pants/option/parser_hierarchy.py
+++ src/python/pants/option/parser_hierarchy.py
@@ -5,6 +5,7 @@
 from __future__ import (nested_scopes, generators, division, absolute_import, with_statement,
                         print_function, unicode_literals)
 
+from pants.option.arg_splitter import GLOBAL_SCOPE
 from pants.option.parser import Parser
 
 
@@ -17,10 +18,11 @@ class ParserHierarchy(object):
   """
   def __init__(self, env, config, all_scopes, legacy_parser=None):
     # Sorting ensures that ancestors precede descendants.
-    all_scopes = sorted(set(list(all_scopes) + ['']))
+    all_scopes = sorted(set(list(all_scopes) + [GLOBAL_SCOPE]))
     self._parser_by_scope = {}
     for scope in all_scopes:
-      parent_parser = None if scope == '' else self._parser_by_scope[scope.rpartition('.')[0]]
+      parent_parser = (None if scope == GLOBAL_SCOPE else
+                       self._parser_by_scope[scope.rpartition('.')[0]])
       self._parser_by_scope[scope] = Parser(env, config, scope, parent_parser, legacy_parser)
 
   def get_parser_by_scope(self, scope):
